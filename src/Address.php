8fa16538e9073f26c83d1948db944065f8a4e4b6
risetechapps <apps@risetech.com.br>
2025-05-02 15:54:27 -0300

    implementado suporte Billing e Delivery

--- src/Address.php
+++ src/Address.php
@@ -7,14 +7,37 @@ use Illuminate\Support\Arr;
 class Address
 {
     protected static array $address = [];
+    protected static array $billing = [];
+    protected static array $delivery = [];
 
     public static function setAddress(array $address): void
     {
-        static::$address=$address;
+        static::$address = $address;
     }
 
-    public static function getAddress(): array{
+    public static function setAddressBilling(array $addressBilling): void
+    {
+        static::$billing = $addressBilling;
+    }
+
+    public static function setAddressDelivery(array $addressDelivery): void
+    {
+        static::$delivery = $addressDelivery;
+    }
+
+    public static function getAddress(): array
+    {
         return static::$address;
+    }
+
+    public static function getAddressBilling(): array
+    {
+        return static::$billing;
+    }
+
+    public static function getAddressDelivery(): array
+    {
+        return static::$delivery;
     }
 
     public static function fillWithDefault($address, $model)
