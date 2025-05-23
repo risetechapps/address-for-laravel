eaf38f859fff801cdc1c96ce4e9de64783de0799
risetechapps <apps@risetech.com.br>
2025-05-23 00:44:26 -0300

    Adicionadas verificações de driver para criação de colunas case-insensitive e extensão citext nas migrações

--- database/migrations/2024_04_01_000000_create_addresses_table.php
+++ database/migrations/2024_04_01_000000_create_addresses_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
 use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -10,17 +9,30 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::createExtensionIfNotExists('citext');
 
-        Schema::create('addresses', function (Blueprint $table) {
+        if(DB::getDriverName() == 'pgsql'){
+            Schema::createExtensionIfNotExists('citext');
+        }
+
+        Schema::create('addresses', function ( $table) {
             $table->uuid('id')->primary();
             $table->uuidMorphs('address');
             $table->string('zip_code', 20)->nullable();
-            $table->caseInsensitiveText('country', 20)->nullable();
-            $table->caseInsensitiveText('state', 20)->nullable();
-            $table->caseInsensitiveText('city', 100)->nullable();
-            $table->caseInsensitiveText('district', 100)->nullable();
-            $table->caseInsensitiveText('address', 255)->nullable();
+
+            if (DB::getDriverName() === 'pgsql') {
+                $table->caseInsensitiveText('country', 20)->nullable();
+                $table->caseInsensitiveText('state', 20)->nullable();
+                $table->caseInsensitiveText('city', 100)->nullable();
+                $table->caseInsensitiveText('district', 100)->nullable();
+                $table->caseInsensitiveText('address', 255)->nullable();
+            } else {
+                $table->string('country', 20)->nullable();
+                $table->string('state', 20)->nullable();
+                $table->string('city', 100)->nullable();
+                $table->string('district', 100)->nullable();
+                $table->string('address', 255)->nullable();
+            }
+
             $table->string('number', 50)->nullable();
             $table->string('complement', 255)->nullable();
             $table->timestamps();
