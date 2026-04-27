3b18e1fb4611f4bb423a1961afa9a4bb2e9dbb47
risetechapps <apps@risetech.com.br>
2026-04-27 15:45:13 -0300

    feat: adiciona billingAddressDefault e métodos de uso

--- src/Traits/HasAddress/HasAddressBilling.php
+++ src/Traits/HasAddress/HasAddressBilling.php
@@ -21,4 +21,25 @@ trait HasAddressBilling
         return $this->morphMany(Address::class, 'address')
             ->where('type', 'BILLING');
     }
+
+    public function billingAddressDefault(): ?Address
+    {
+        return $this->addressBilling()->default()->first();
+    }
+
+    /**
+     * Get the most used billing addresses.
+     */
+    public function mostUsedBillingAddresses(int $limit = 5)
+    {
+        return $this->addressBilling()->mostUsed($limit)->get();
+    }
+
+    /**
+     * Get the most used billing address.
+     */
+    public function mostUsedBillingAddress(): ?Address
+    {
+        return $this->addressBilling()->mostUsed(1)->first();
+    }
 }
